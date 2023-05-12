be2e6b338e29ed91dd51b8b53947f32238031c92
Brayhan De Aza <brayhandeaza@gmail.com>
2023-05-11 23:54:42 -0400

    landing page has image and button but not functionanlity

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import Button from './Button'
+import LandingImg from "../assets/img/landing.png"
 
 const LandingPage = () => {
-    const onClick = (title) => {
-        alert(title)
-    }
-  return (
-    <div className='LandingPage'>
-          {/* <Button onClick={onClick} title={"Student"} />
-          <Button onClick={()=> onClick("working")} title={"District"}/> */}
 
-    </div>
-  )
+	const onClickButton = (e) => {
+		console.log(e.target.innerText)
+	}
+
+	return (
+		<div className='LandingPage' >
+			<img src={LandingImg} alt="LandingImg" />
+			<div className="info">
+				<h1 >Welcome to Spin and Learn  <br />Table Tennis For All Schools</h1>
+				<div className="buttons">
+					<Button width={120} onClick={onClickButton} filled={false} title={"District"} />
+					<Button width={120} onClick={onClickButton} filled={false} title={"Students"} />
+					<Button width={120} onClick={onClickButton} filled={false} title={"Leaders"} />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default LandingPage
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,10 +1,9 @@
 import { Space } from 'antd'
-import React, { useContext , useEffect, useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { MainContext } from '../contexts/MainContext'
 
 const NavLinks = () => {
     const { isLoggedIn } = useContext(MainContext)
-    
     const [pathname, setPathname] = useState("/")
 
     const links = [
@@ -15,19 +14,17 @@ const NavLinks = () => {
     ]
 
     useEffect(() => {
-        console.log(window.location.pathname)
         setPathname(window.location.pathname)
-     },[window.location.pathname])
+    }, [window.location.pathname])
 
     return (
         <div className="nav-links">
             {links.map((link, key) => (
-                <a href={link.path} key={`nav-links-${link.path}-${key}`} style={{ color: pathname === "/" + link.path? "rgba(9, 114, 206, 0.7)":"#0972CE"}}>{link.name} </a>
-            
+                <a className="hover" href={link.path} key={`nav-links-${link.path}-${key}`} style={{ color: pathname === "/" + link.path ? "rgba(9, 114, 206, 0.7)" : "#0972CE" }}>{link.name} </a>
+
             ))}
-            {!isLoggedIn && <a className="hover" href={"signup"} style={{ color: pathname === "/signup" ? "rgba(9, 114, 206, 0.7)":"#0972CE"}}> Sign Up </a>}
+            {!isLoggedIn && <a className="hover" href={"signup"} style={{ color: pathname === "/signup" ? "rgba(9, 114, 206, 0.7)" : "#0972CE" }}> Sign Up </a>}
             {!isLoggedIn && <a className="hover" href={"login"} style={{ color: pathname === "/login" ? "rgba(9, 114, 206, 0.7)" : "#0972CE" }}> Log In </a>}
-            
         </div>
     )
 }
